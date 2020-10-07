095ab63f4917997ef6ec64e9f2c7d3c43d1ef434
Gustavo Souza <gtssouza94@gmail.com>
2020-10-06 21:19:11 -0300

    Trocando os frames do sprite

--- src/jogo/Cenario1.java
+++ src/jogo/Cenario1.java
@@ -7,11 +7,13 @@ import jplay.Window;
 public class Cenario1 {
     private Window janela;
     private Scene cena;
+    private Jogador jogador;
 
     public Cenario1(Window window){
         janela = window;
         cena = new Scene();
         cena.loadFromFile(URL.scenario("Cenario1.scn"));
+        jogador = new Jogador(640, 350);
 
         run();
     }
@@ -19,6 +21,8 @@ public class Cenario1 {
     private void run() {
         while(true){
             cena.draw();
+            jogador.draw();
+            jogador.mover(janela);
             janela.update();
         }
     }
